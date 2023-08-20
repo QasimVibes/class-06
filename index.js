8322f823d1573a3d7d514a6cafc6981f66b06035
QasimVibes <qasimrazzaq921@gmail.com>
2023-08-20 17:27:15 +0500

    Task done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
+// Write a program sum of all numbers between 1 to 100
 let plus = 0;
 let list = [];
 for (let index = 1; index <= 100; index++) {
@@ -8,3 +9,20 @@ for (let index = 1; index <= 100; index++) {
     }
 }
 console.log("Sum ", plus, list);
+let factNum = 10;
+const factorial = (factNum) => {
+    if (factNum > 0) {
+        let result = 1;
+        let index = 1;
+        while (index <= factNum) {
+            result *= index;
+            index++;
+        }
+        return result;
+    }
+    else {
+        throw new Error("Please enter a positive number");
+    }
+};
+let factResult = factorial(factNum);
+console.log(factResult);
